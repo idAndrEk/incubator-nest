31a4576337b1dc794dd19db607d47b3e907ff786
Andrey <AndrEkat96@yandex.ru>
2022-12-02 09:38:44 +0500

    password

--- src/services/passwordService/password.service.ts
+++ src/services/passwordService/password.service.ts
@@ -1,16 +1,17 @@
-import bcrypt from "bcryptjs";
-// import bcrypt from 'bcrypt';
+// import bcrypt from "bcryptjs";
+import bcrypt from 'bcrypt';
 // import * as bcrypt from 'bcrypt';
 import { Injectable } from "@nestjs/common";
+// const {bcrypt} = require('bcryptjs');
+
 
 @Injectable()
 export class PasswordService {
-  async isMatch(password: string, hash: string): Promise<boolean> {
+    async isMatch(password: string, hash: string): Promise<boolean> {
     return bcrypt.compare(password, hash);
   }
 
   async hashPassword(password: string): Promise<string> {
-    const {bcrypt} = require('bcryptjs');
     const salt = 10;
     return await bcrypt.hash(password, salt);
   }
