4c700b3d003772547d440885272f5d9a8651d547
Andrey <AndrEkat96@yandex.ru>
2022-12-08 14:49:24 +0500

    comment delete

--- src/comment/comment.providers.ts
+++ src/comment/comment.providers.ts
@@ -1,8 +1,8 @@
-import { Mongoose } from "mongoose";
-import { CommentSchema } from "./commentSchema";
-
-export const commentProviders=[{
-  provide:"COMMENT_MODEL",
-  useFactory: (mongoose: Mongoose) => mongoose.model("Comments", CommentSchema),
-  inject: ["DATABASE_CONNECTION"]
-}]
+// import { Mongoose } from "mongoose";
+// import { CommentSchema } from "./commentSchema";
+//
+// export const commentProviders=[{
+//   provide:"COMMENT_MODEL",
+//   useFactory: (mongoose: Mongoose) => mongoose.model("Comments", CommentSchema),
+//   inject: ["DATABASE_CONNECTION"]
+// }]
