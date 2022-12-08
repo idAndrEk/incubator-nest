c774f93bf7d2ae1c36be066df419403d693d8aa6
Andrey <AndrEkat96@yandex.ru>
2022-12-08 14:47:33 +0500

    comment delete

--- src/comment/comment.module.ts
+++ src/comment/comment.module.ts
@@ -1,13 +1,13 @@
-import { Module } from "@nestjs/common";
-import { DatabaseModule } from "../database/database.module";
-import { commentProviders } from "./comment.providers";
-import { CommentsController } from "./commet.controller";
-
-@Module({
-  imports: [DatabaseModule],
-  controllers: [CommentsController],
-  providers: [...commentProviders, ],
-  exports: []
-})
-export class CommentModule {
-}
+// import { Module } from "@nestjs/common";
+// import { DatabaseModule } from "../database/database.module";
+// import { commentProviders } from "./comment.providers";
+// import { CommentsController } from "./commet.controller";
+//
+// @Module({
+//   imports: [DatabaseModule],
+//   controllers: [CommentsController],
+//   providers: [...commentProviders, ],
+//   exports: []
+// })
+// export class CommentModule {
+// }
--- src/comment/commet.controller.ts
+++ src/comment/commet.controller.ts
@@ -1,34 +1,34 @@
-import { CommentsQueryRepository } from "./comment.query-repostory";
-import { CommentsService } from "./comment.service";
-import { Body, Get, HttpCode, HttpStatus, Injectable, NotFoundException, Param, Put, Req } from "@nestjs/common";
-import { ObjectId } from "mongodb";
-import { UserViewResponse } from "../users/types/usersType";
-import { commentDto } from "./dto/commentDto";
-
-@Injectable()
-export class CommentsController {
-
-  constructor(protected commentsService: CommentsService,
-              protected commentsQueryRepository: CommentsQueryRepository) {
-  }
-
-  @Get(":id")
-  @HttpCode(HttpStatus.OK)
-  async getComment(
-    @Param("id") id: ObjectId,
-    @Req() user: UserViewResponse) {
-    return await this.commentsQueryRepository.getComment(id, user);
-  }
-
-  @Put(":id")
-  @HttpCode(HttpStatus.OK)
-  async updateComment(
-    @Param("id") id: ObjectId,
-    @Body() updateComment: commentDto
-  ) {
-    const checkComment = await this.commentsQueryRepository.checkComment(id);
-    if (!checkComment) throw new NotFoundException("Comment does not exist!");
-    const updateResult = await this.commentsService.updateComment(id, updateComment);
-    return updateResult;
-  }
-}
+// import { CommentsQueryRepository } from "./comment.query-repostory";
+// import { CommentsService } from "./comment.service";
+// import { Body, Get, HttpCode, HttpStatus, Injectable, NotFoundException, Param, Put, Req } from "@nestjs/common";
+// import { ObjectId } from "mongodb";
+// import { UserViewResponse } from "../users/types/usersType";
+// import { commentDto } from "./dto/commentDto";
+//
+// @Injectable()
+// export class CommentsController {
+//
+//   constructor(protected commentsService: CommentsService,
+//               protected commentsQueryRepository: CommentsQueryRepository) {
+//   }
+//
+//   @Get(":id")
+//   @HttpCode(HttpStatus.OK)
+//   async getComment(
+//     @Param("id") id: ObjectId,
+//     @Req() user: UserViewResponse) {
+//     return await this.commentsQueryRepository.getComment(id, user);
+//   }
+//
+//   @Put(":id")
+//   @HttpCode(HttpStatus.OK)
+//   async updateComment(
+//     @Param("id") id: ObjectId,
+//     @Body() updateComment: commentDto
+//   ) {
+//     const checkComment = await this.commentsQueryRepository.checkComment(id);
+//     if (!checkComment) throw new NotFoundException("Comment does not exist!");
+//     const updateResult = await this.commentsService.updateComment(id, updateComment);
+//     return updateResult;
+//   }
+// }
